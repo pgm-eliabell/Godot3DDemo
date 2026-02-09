3f63fc9606d5e61914c03c0eb49a6286ad573da8
pgm-eliabell <eliabell@student.arteveldehs.be>
2026-02-10 00:17:28 +0100

    deleted old animation logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -32,28 +32,31 @@ func _input(event):
 		camera_mount.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
 
 func _physics_process(delta: float) -> void:
-	if !animation_player.is_playing():
-		is_attacking = false
-		is_blocking = false
+	#if !animation_player.is_playing():
+		#is_attacking = false
+		#is_blocking = false
 	
 	#if Input.is_action_just_pressed("attack"):
 		#if animation_player.current_animation != "1H_Melee_Attack_Slice_Horizontal":
 			#animation_player.play("1H_Melee_Attack_Slice_Horizontal")
 			#is_attacking = true
 			
-	if Input.is_action_just_pressed("block"):
-		if animation_player.current_animation != "blockStance":
-			animation_player.play("BlockStance")
-			is_blocking = true
+	#if Input.is_action_just_pressed("block"):
+		#if animation_player.current_animation != "blockStance":
+			#animation_player.play("BlockStance")
+			#is_blocking = true
 		
 	if Input.is_action_pressed("shift"):
 		SPEED = running_speed
 		running = true
+		print("were running")
 	else: 
 		SPEED = walking_speed
 		running = false
+		#print("were walking")
 	# Add the gravity.
 	if not is_on_floor():
+		print("where not on the floor")
 		velocity += get_gravity() * delta
 
 	# Handle jump.
@@ -65,12 +68,12 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if running: 
-			if animation_player.current_animation != "Running":
-				animation_player.play("Running")
-		else:
-			if animation_player.current_animation != "Walking":
-				animation_player.play("Walking")
+		#if running: 
+			#if animation_player.current_animation != "Running":
+				#animation_player.play("Running")
+		#else:
+			#if animation_player.current_animation != "Walking":
+				#animation_player.play("Walking")
 
 		#debug_timer += delta
 		#if debug_timer >= 0.5:
@@ -78,14 +81,13 @@ func _physics_process(delta: float) -> void:
 			#debug_timer = 0
 		character_visual.look_at(position + -direction)
 		debug_arrow.look_at(position + direction)
-			
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		
 	else:
-		if !is_attacking and !is_blocking:
-			if animation_player.current_animation != "Idle":
-				animation_player.play("Idle")
+		#if !is_attacking and !is_blocking:
+			#if animation_player.current_animation != "Idle":
+				#animation_player.play("Idle")
 					
 				velocity.x = move_toward(velocity.x, 0, SPEED)
 				velocity.z = move_toward(velocity.z, 0, SPEED)
